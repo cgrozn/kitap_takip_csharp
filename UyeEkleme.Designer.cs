8cca819d6f78b87e19b46ae70399173d83fbbcff
ÇAĞRI <33495266+cgrozn@users.noreply.github.com>
2019-09-22 01:36:20 +0300

    Create UyeEkleme.Designer.cs

---
+++ UyeEkleme.Designer.cs
@@ -0,0 +1,223 @@
+namespace gorselodevim
+{
+    partial class UyeEkleme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.kullanici_adi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kullanici_pass = new System.Windows.Forms.TextBox();
+            this.kullanici_tel = new System.Windows.Forms.TextBox();
+            this.kullanici_adres = new System.Windows.Forms.TextBox();
+            this.kullanici_cins = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_ekle_uye = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.kullanici_mail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // kullanici_adi
+            // 
+            this.kullanici_adi.Location = new System.Drawing.Point(216, 62);
+            this.kullanici_adi.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_adi.Name = "kullanici_adi";
+            this.kullanici_adi.Size = new System.Drawing.Size(162, 26);
+            this.kullanici_adi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parola";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Adres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefon";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cinsiyet";
+            // 
+            // kullanici_pass
+            // 
+            this.kullanici_pass.Location = new System.Drawing.Point(216, 115);
+            this.kullanici_pass.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_pass.Name = "kullanici_pass";
+            this.kullanici_pass.Size = new System.Drawing.Size(162, 26);
+            this.kullanici_pass.TabIndex = 6;
+            // 
+            // kullanici_tel
+            // 
+            this.kullanici_tel.Location = new System.Drawing.Point(216, 172);
+            this.kullanici_tel.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_tel.Name = "kullanici_tel";
+            this.kullanici_tel.Size = new System.Drawing.Size(162, 26);
+            this.kullanici_tel.TabIndex = 7;
+            // 
+            // kullanici_adres
+            // 
+            this.kullanici_adres.Location = new System.Drawing.Point(216, 233);
+            this.kullanici_adres.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_adres.Name = "kullanici_adres";
+            this.kullanici_adres.Size = new System.Drawing.Size(162, 26);
+            this.kullanici_adres.TabIndex = 8;
+            // 
+            // kullanici_cins
+            // 
+            this.kullanici_cins.FormattingEnabled = true;
+            this.kullanici_cins.Items.AddRange(new object[] {
+            "ERKEK",
+            "KADIN"});
+            this.kullanici_cins.Location = new System.Drawing.Point(216, 328);
+            this.kullanici_cins.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_cins.Name = "kullanici_cins";
+            this.kullanici_cins.Size = new System.Drawing.Size(159, 27);
+            this.kullanici_cins.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 397);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 33);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "İPTAL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_ekle_uye
+            // 
+            this.btn_ekle_uye.AccessibleDescription = "";
+            this.btn_ekle_uye.Location = new System.Drawing.Point(206, 397);
+            this.btn_ekle_uye.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btn_ekle_uye.Name = "btn_ekle_uye";
+            this.btn_ekle_uye.Size = new System.Drawing.Size(99, 33);
+            this.btn_ekle_uye.TabIndex = 12;
+            this.btn_ekle_uye.Text = "EKLE";
+            this.btn_ekle_uye.UseVisualStyleBackColor = true;
+            this.btn_ekle_uye.Click += new System.EventHandler(this.btn_ekle_uye_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 19);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Mail";
+            // 
+            // kullanici_mail
+            // 
+            this.kullanici_mail.Location = new System.Drawing.Point(216, 275);
+            this.kullanici_mail.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.kullanici_mail.Name = "kullanici_mail";
+            this.kullanici_mail.Size = new System.Drawing.Size(162, 26);
+            this.kullanici_mail.TabIndex = 14;
+            // 
+            // UyeEkleme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(650, 458);
+            this.Controls.Add(this.kullanici_mail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_ekle_uye);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kullanici_cins);
+            this.Controls.Add(this.kullanici_adres);
+            this.Controls.Add(this.kullanici_tel);
+            this.Controls.Add(this.kullanici_pass);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kullanici_adi);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "UyeEkleme";
+            this.Text = "UyeEkleme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox kullanici_adi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox kullanici_pass;
+        private System.Windows.Forms.TextBox kullanici_tel;
+        private System.Windows.Forms.TextBox kullanici_adres;
+        private System.Windows.Forms.ComboBox kullanici_cins;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_ekle_uye;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox kullanici_mail;
+    }
+}
